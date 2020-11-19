867b16d78b4874f70a6abf337a0c851c77e41510
Drovenger <64911983+Drovenger@users.noreply.github.com>
2020-11-19 17:28:36 +0700

    Python_If_Else.py

--- main/Python_If_Else.py
+++ main/Python_If_Else.py
@@ -4,5 +4,13 @@ import random
 import re
 import sys
 
+
+
 if __name__ == '__main__':
     n = int(raw_input().strip())
+if n % 2 !=0:
+    print("Weird")
+elif n >= 6 and n<= 20:
+    print("Weird")
+else:
+    print("Not Weird")
